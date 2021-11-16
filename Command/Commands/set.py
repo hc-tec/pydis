cbc45f40baa01c5a5d4c22eb88754d8760a746f3
hc-tec <2173324540@qq.com>
2021-11-16 10:09:28 +0800

    expires dict init to database

--- Command/Commands/set.py
+++ Command/Commands/set.py
@@ -1,6 +1,9 @@
 
 from Command import BaseCommand
 from Database import Database
+from Timer.event import TimeoutEvent
+from Timer.timestamp import Timestamp
+# from Server.server import server
 
 
 class Set(BaseCommand):
@@ -10,9 +13,31 @@ class Set(BaseCommand):
 
     def handle(self, args):
         db: Database = self.client.db
-        expires_time = args.get('expires_time')
+        expires_time = int(args.get('expires_time'))
         if expires_time is None:
             db.store(args['key'], args['value'])
         else:
-            pass
+            db.store(args['key'], args['value'])
+            db.store_expires(args['key'], expires_time)
+            self.set_expires_timer(args['key'], expires_time)
 
+    def set_expires_timer(self, key, expires_time):
+        timestamp = Timestamp(expires_time, 's')
+        timeout_event = TimeoutEvent(timestamp)
+        timeout_event.set_callback(Set.expires_event).set_extra_data({
+            "client": self.client,
+            "expires_key": key
+        })
+        server = self.client.get_server()
+        reactor = server.get_loop()
+        reactor.create_timeout_event(timeout_event)
+        print('expire event build')
+
+    @staticmethod
+    def expires_event(reactor, extra_data):
+        print('expire event activate')
+        client = extra_data['client']
+        db: Database = client.db
+        print(db.expires, db.dict)
+        db.remove_expires(extra_data['expires_key'])
+        print(db.expires, db.dict)
--- Database/database.py
+++ Database/database.py
@@ -11,7 +11,22 @@ class Database:
 
     def store(self, key, value):
         self.dict[key] = value
-    
+
     def withdraw(self, key):
         return self.dict.get(key)
-    
+
+    def remove(self, key):
+        if self.withdraw(key):
+            del self.dict[key]
+
+    def store_expires(self, key, expires_time):
+        self.expires[key] = expires_time
+
+    def withdraw_expires(self, key):
+        return self.expires.get(key)
+
+    def remove_expires(self, key):
+        if self.withdraw_expires(key):
+            del self.expires[key]
+        self.remove(key)
+
